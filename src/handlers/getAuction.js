7aa5fccb6ea1b5f9906e553202b304b3ceeed4e1
Rishabh Jain <rishabhjain94@bitbucket.org>
2020-12-17 12:30:55 +0530

    added a function to place bids

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -4,27 +4,32 @@ import createError from 'http-errors'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
+export async function getAuctionById(id) {
+  let auction
+  try {
+    const result = await dynamodb
+      .get({ TableName: process.env.AUCTIONS_TABLE_NAME, Key: { id } })
+      .promise()
+    auction = result.Item
+  } catch (error) {
+    console.error(error)
+    throw new createError.InternalServerError(error)
+  }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ${id} not found!`)
+  }
+  return auction
+}
+
 async function getAuctions(event, context) {
-    const { id } = event.pathParameters;
-    let auction;
-    try {
-        const result = await dynamodb
-        .get({ TableName: process.env.AUCTIONS_TABLE_NAME, Key: {id} })
-        .promise()
-        auction = result.Item;
-    } catch (error) {
-        console.error(error)
-        throw new createError.InternalServerError(error)
-    }
+  const { id } = event.pathParameters
+  const auction = await getAuctionById(id)
 
-    if(!auction){
-        throw new createError.NotFound({message: `Auction with ${id} not found!`});
-    }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(auction),
-    }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  }
 }
 
 export const handler = commonMiddlware(getAuctions)
