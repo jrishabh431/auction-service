4a35a3d378f420b30cc360720cc53288747b8a9d
Rishabh Jain <rishabhjain94@bitbucket.org>
2020-12-20 03:02:13 +0530

    added validation and changed to query from scan for getAuctions

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,15 +1,38 @@
 import AWS from 'aws-sdk'
 import commonMiddlware from '../lib/commonMiddleware'
+import jsonAuctionStatusSchema from '../lib/schemas/getAuctionsSchema'
 import createError from 'http-errors'
+import validator from '@middy/validator'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 async function getAuctions(event, context) {
   let auctions
+  // we will get this qs and will query on it
+  const { status } = event.queryStringParameters
+
   try {
-    const result = await dynamodb
-      .scan({ TableName: process.env.AUCTIONS_TABLE_NAME })
-      .promise()
+    // with the below we will get all the auctions available, but since scan is costly and we only want
+    // to fetch all the open auctions or based on certain status we can use query, it will be cheaper as well
+
+    // const result = await dynamodb
+    //   .scan({ TableName: process.env.AUCTIONS_TABLE_NAME })
+    //   .promise()
+    // auctions = result.Items
+
+    // searching with query
+    const params = {
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      IndexName: 'statusAndEndDate',
+      KeyConditionExpression: `#status = :status`,
+      ExpressionAttributeValues: {
+        ':status': status,
+      },
+      ExpressionAttributeNames: {
+        '#status': 'status',
+      },
+    }
+    const result = await dynamodb.query(params).promise()
     auctions = result.Items
   } catch (error) {
     console.error(error)
@@ -22,4 +45,6 @@ async function getAuctions(event, context) {
   }
 }
 
-export const handler = commonMiddlware(getAuctions)
+export const handler = commonMiddlware(getAuctions).use(
+  validator({ inputSchema: jsonAuctionStatusSchema, useDefaults: true })
+)
