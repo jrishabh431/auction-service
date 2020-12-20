5570261ad4e67402c69f7cd0153d92d05c781b45
Rishabh Jain <rishabhjain94@bitbucket.org>
2020-12-20 21:39:06 +0530

    configured S3 Bucket

---
+++ src/handlers/uploadAuctionPicture.js
@@ -0,0 +1,9 @@
+async function uploadAuctionPicture() {
+    return{
+        statusCode: 200,
+        body: JSON.stringify({})
+    }
+
+}
+
+export const handler = uploadAuctionPicture
--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,21 +1,62 @@
-import AWS from 'aws-sdk';
+import AWS from 'aws-sdk'
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+const sqs = new AWS.SQS()
 
-export const closeAuction = async(auction) => {
-    const params = {
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-        Key: { id: auction.id },
-        UpdateExpression: `set #status = :status`,
-        ExpressionAttributeValues: {
-            ':status': 'CLOSED'
-        },
-        ExpressionAttributeNames: {
-            '#status': 'status'
-        }
-        // ExpressionAttributeNames is used to define words where # is used in expression
-        // # is used to escape the reserverd words, status is a reserved word in aws expression
-      }
-      const result = await dynamodb.update(params).promise()
-      return result
+export const closeAuction = async (auction) => {
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id: auction.id },
+    UpdateExpression: `set #status = :status`,
+    ExpressionAttributeValues: {
+      ':status': 'CLOSED',
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+    // ExpressionAttributeNames is used to define words where # is used in expression
+    // # is used to escape the reserverd words, status is a reserved word in aws expression
+  }
+  await dynamodb.update(params).promise()
+
+  const { title, seller, highestBid } = auction
+  const { amount, bidder } = highestBid
+
+  if (amount == 0) {
+    const notifySellerNoSell = await sqs
+      .sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+          subject: 'No bids on your auction item :(',
+          recipient: seller,
+          body: `Oh no! Your item "${title}" didn't get any bids. Better luck next time!`,
+        }),
+      })
+      .promise()
+    return notifySellerNoSell
+  }
+
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'Your item has been sold!',
+        recipient: seller,
+        body: `Woohoo! Your item "${title}" has been sold for ${amount} Rupees.`,
+      }),
+    })
+    .promise()
+
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'You won an auction!',
+        recipient: bidder,
+        body: `What a great deal! You got yourself a "${title}" for ${amount} Rupees.`,
+      }),
+    })
+    .promise()
+
+  return Promise.all([notifySeller, notifyBidder])
 }
